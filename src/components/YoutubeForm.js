787b7d23f83b6ccb29074b9bf38dd51664bfcdbe
Eshwar Deshmukh <deshmukh.eshwar@gmail.com>
2023-08-30 18:46:22 +0530

    Added with Validation

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,20 +1,77 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { DevTool } from '@hookform/devtools'
 
 function YoutubeForm() {
+  const form = useForm({
+    defaultValues: {
+      username: "Eshwar",
+      email: "",
+      channel: ""
+    }
+  })
+
+  const {register, control, handleSubmit, formState} = form
+  const { errors } = formState
+
+  // Approach 1 - to register form control with react-hook-form hook
+  // const {name, ref, onChange, onBlur} = register("username")
+
+  const onSubmit = (data) =>{
+    console.log('Form submitted :- \n', data);
+  }
+
+  const notAdmin = (fieldValue) => {
+    return fieldValue != "admin@gmail.com" || 
+    'Enter a different email address'
+  }
+  const notBlackListed = (fieldValue) => {
+    return !fieldValue.endsWith('notvalid.com') ||
+    'This domain is not supported'
+  } 
   return (
     <div className='youtubeForm'>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <div className='form-control'>
             <label htmlFor="Username">User Name</label>
-            <input type="text" name="username" id="username" />
+            {/* Approach 1  */}
+            {/* <input type="text" name={name} ref={ref} onChange={onChange} onBlur={onBlur} /> */}
+            {/* Approach 2 */}
+            <input type="text" id='username' {...register("username", {
+              required: {
+                value: true,
+                message: 'Username is required'
+              }
+            })} />
+            <p className='errorP'>{errors.username?.message}</p>
+            </div>
 
+            <div className='form-control'>
             <label htmlFor="email">E-mail</label>
-            <input type="email" name="email" id="email" />
+            <input type="email" id="email"  {...register("email", {
+              pattern: {
+                value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                message: 'Invalid email format',
+              },
+              validate: {notAdmin, notBlackListed}
+            })} />
+            <p className='errorP'>{errors.email?.message}</p>
+            </div>
 
+            <div className='form-control'>
             <label htmlFor="channel">Channel</label>
-            <input type="text" name="channel" id="channel" />
+            <input type="text" id="channel"  {...register("channel", {
+              required: {
+                value: true,
+                message: 'Channel name is required'
+              }
+            })} />
+            <p className='errorP'>{errors.channel?.message}</p>
+            </div>
 
             <button>Submit</button>
         </form>
+        <DevTool control={control}/>
     </div>
   )
 }
